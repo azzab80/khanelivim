17cd0c36e5607fad3cf9c4bfa71a843800c55332
Aaron Bird <aaorn.bird@briefbox.com>
2024-08-22 19:17:24 +1000

    add copilot

---
+++ packages/khanelivim/plugins/copilot-cmp.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  plugins.copilot-cmp = {
+    enable = true;
+  };
+  plugins.copilot-lua = {
+    enable = true;
+    suggestion = {
+      enabled = false;
+    };
+    panel = {
+      enabled = false;
+    };
+  };
+
+  extraConfigLua = ''
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    })
+  '';
+}
